d486319502b95d17bfcae76efeb3e32389337340
manuellpz <manuelmzt44@gmail.com>
2022-12-13 16:37:27 -0700

    Add Button

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 let txt_search = document.querySelector('#txt_search');
 let iframe = document.querySelector('#ifResult');
 let option = document.querySelector('#mp3mp4');
+const btn = document.querySelector('#btn_getMedia');
+
+function getMedia(url) {
+    iframe.src = `https://convert2mp3s.com/api/single/${option.value}?url=${url}`;
+}
 
 txt_search.addEventListener('keyup', (e) => {
 	if(e.keyCode == 13) {
-      	let url = e.target.value;
-        iframe.src = `https://convert2mp3s.com/api/single/${option.value}?url=${url}`;
+      	getMedia(e.target.value);
     }
   
 });
+btn.addEventListener('click',()=> {
+    if(txt_search.value != "")
+        getMedia(txt_search.value);
+})
